4f9179473c3dfdfc340e342840f34b0b700eec56
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-23 11:31:03 -0800

    morning work - Level 0 done, Level 1 in progress

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 
-const App = (props) => (
-  <div>Hello World!</div>
+
+var sampleMovies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+const SearchBar = (props) => {
+
+  return (
+    <form>
+      <input type="text"></input>
+
+    </form>
+  )
+}
+
+
+const MovieItem = (props) => (
+  <div className="movie">
+    <li>{props.movie.title}</li>
+  </div>
+)
+const MovieList = (props) => {
+
+  return (
+    <ul>
+      {props.moviesList.map((movie) => (
+        <MovieItem key={movie.title} movie={movie} />
+      ))}
+    </ul>
+  )
+};
+
+const App = (props) => {
+  const [moviesList, setMoviesList] = React.useState(sampleMovies);
+  // console.log(moviesList);
+  return (
+  <div>
+    <div className="topbar">
+    <h1>Movie List</h1>
+    </div>
+    <SearchBar />
+    <MovieList moviesList={moviesList} />
+  </div>
 );
+};
 
 export default App;
